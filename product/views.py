b32dc3f9f4eb884fb9cce40e30c83cb2ed3b47a8
psw <psw03180@naver.com>
2025-07-11 17:20:18 +0900

    2주차 django 개발완료

---
+++ product/views.py
@@ -0,0 +1,12 @@
+from django.shortcuts import render
+from .models import MainContent
+
+
+def mainpage(request):
+    return render(request, 'mainpage.html')
+
+def index(request):
+
+    content_list = MainContent.objects.order_by('-pub_date')
+    context = {'content_list':content_list}
+    return render(request, 'product/content_list.html', context)
